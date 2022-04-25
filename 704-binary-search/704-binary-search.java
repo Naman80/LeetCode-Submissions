00e72d224994670c4225a25ba176f2994f537ac2
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-04-25 11:00:54 +0530

    Time: 0 ms (100.00%), Space: 53.9 MB (67.33%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int j = nums.length-1;
+        int i = 0;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid] > target) j = mid-1;
+            else i = mid+1;
+        }
+        
+        return -1;
+    }
+}
