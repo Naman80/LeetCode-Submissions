ca60561505540382c6936cefe3006918afeff25c
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-04-30 13:28:36 +0530

    Time: 7 ms (80.31%), Space: 45.2 MB (12.75%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,24 +1,48 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        int ans  = 0;
-        for(int i = 0;i<n;i++){
-            int max = 0;
-            for(int j = 0;j<i;j++){
+    // public int lengthOfLIS(int[] nums) {
+    //     int n = nums.length;
+    //     int[] dp = new int[n];
+    //     int ans  = 0;
+    //     for(int i = 0;i<n;i++){
+    //         int max = 0;
+    //         for(int j = 0;j<i;j++){
+    //             if(nums[i] > nums[j]){
+    //                 max = Math.max(dp[j],max);
+    //             }   
+    //         }  
+    //         dp[i] = max+1;
+    //         ans = Math.max(ans,dp[i]);
+    //     }
+    //     return ans;
+    // }
+    
+    
+     public int lengthOfLIS(int[] nums) {
+         
+         int n = nums.length;
+         
+         ArrayList<Integer> al = new ArrayList<>();
+         
+         al.add(nums[0]);
+         
+         for(int i = 1;i<n;i++){
+             
+             if(nums[i] > al.get(al.size()-1)){
+                 al.add(nums[i]);
+             }else{
+                 
+                 int pos =  Collections.binarySearch(al,nums[i]);
+                 if(pos<0){
+                     pos = Math.abs(pos+1);
+                        al.set(pos,nums[i]);
+                 }
                 
-                if(nums[i] > nums[j]){
-                    max = Math.max(dp[j],max);
-                }
-                
-            }
-           
-            dp[i] = max+1;
-             ans = Math.max(ans,dp[i]);
-        }
-        
-        return ans;
-        
-        
-    }
+                    
+             }
+             
+         }
+         
+         return al.size();
+         
+     }
 }
