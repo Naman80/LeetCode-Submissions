54aa31ad554f738f2198f82a81c0e807d41e4922
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-11 08:47:25 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (23.34%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,23 +1,33 @@
 class Solution {
     public int countVowelStrings(int n) {
-        int[][] dp = new int[6][n+1];
-        for(int i = 0;i<6;i++){
-            for(int j = 0;j<n+1;j++){
-                dp[i][j] = -1;
+        int[]dp = new int[5];
+        for(int i = 0 ;i<5;i++){
+            dp[i] = 1;
+        }
+        
+        for(int i = 1 ; i<=n;i++){
+            for(int j = 3 ; j>=0;j--){
+                dp[j] = dp[j] + dp[j+1];
             }
         }
-        char[] vo = {'a','e','i','o','u'};
-        return solve(0,vo,n,"",dp);
+        
+        return dp[0];
+        // for(int i = 0;i<6;i++){
+        //     for(int j = 0;j<n+1;j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // char[] vo = {'a','e','i','o','u'};
+        // return solve(0,vo,n,"",dp);
     }
     
-    public int solve(int idx,char[] vo, int n,String p,int[][]dp){
-        if(p.length()==n)return 1;
-        if(idx==vo.length) return 0;
-        if(dp[idx][p.length()] != -1)return dp[idx][p.length()];
-        int pick = solve(idx,vo,n,p+vo[idx],dp);
-        int np = solve(idx+1,vo,n,p,dp);
-        return dp[idx][p.length()] = np+pick;
-    }
-    
-    
+    // public int solve(int idx,char[] vo, int n,String p,int[][]dp){
+    //     if(p.length()==n)return 1;
+    //     if(idx==vo.length) return 0;
+    //     if(dp[idx][p.length()] != -1)return dp[idx][p.length()];
+    //     int pick = solve(idx,vo,n,p+vo[idx],dp);
+    //     int np = solve(idx+1,vo,n,p,dp);
+    //     return dp[idx][p.length()] = np+pick;
+    // }
+
 }
