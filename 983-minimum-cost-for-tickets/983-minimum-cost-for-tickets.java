b4861d7f931bfb48a62eb7e65346fd9873237432
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-21 10:09:42 +0530

    Time: 8 ms (8.20%), Space: 48.2 MB (5.25%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,34 +1,27 @@
 class Solution {
+    int[] days,costs;
+    int[][] dp;
     public int mincostTickets(int[] days, int[] costs) {
+        this.days = days;
+        this.costs = costs;
         int n = days.length;
-        int[][] dp = new int[n][days[n-1]+30];
+        dp = new int[n][days[n-1]+30];
         for(int[] x:dp)Arrays.fill(x,-1);
-        return solve(0,0,days,costs,dp);
+        return solve(0,0);
     }
     
     
-    static int solve(int idx ,int next ,int[] days,int[] cost,int[][] dp){
+     int solve(int idx ,int next){
         if(idx>=days.length)return 0;
         if(dp[idx][next]!=-1)return dp[idx][next];
         if(days[idx]>next){
-             int cal = Integer.MAX_VALUE;
-            for(int i = 0;i<3;i++){
-               
-                if(i==0){
-                    cal = Math.min(cost[i] + solve(idx+1,days[idx],days,cost,dp),cal);
-                } 
-                else if(i==1){
-                    cal = Math.min(cost[i] + solve(idx+1,days[idx]+6,days,cost,dp),cal);
-                }else{
-                    cal = Math.min(cost[i] + solve(idx+1,days[idx]+29,days,cost,dp),cal);
-                }
-                
-                
-            }
+             int cal = Integer.MAX_VALUE;               
+                    cal = Math.min(costs[0] + solve(idx+1,days[idx]),cal);
+                    cal = Math.min(costs[1] + solve(idx+1,days[idx]+6),cal);
+                    cal = Math.min(costs[2] + solve(idx+1,days[idx]+29),cal);
             return dp[idx][next] = cal;
         } 
-        
-        return dp[idx][next] =  solve(idx+1,next,days,cost,dp);
+        return dp[idx][next] =  solve(idx+1,next);
         
         
         
