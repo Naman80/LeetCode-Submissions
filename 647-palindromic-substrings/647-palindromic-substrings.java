cf2521d2fd5ad4b5e12081ac1d0631a9edbd1bdf
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-22 20:20:53 +0530

    Time: 561 ms (10.25%), Space: 146.3 MB (5.19%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countSubstrings(String s) {
+        
+       int n = s.length();
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+          for(int j = i;j<n;j++){
+              if(isPal(s.substring(i,j+1)))sum++;
+          }
+        }
+        return sum;
+    }
+    
+    
+    
+    boolean isPal(String s){
+        
+        int i =0;
+        int j = s.length()-1;
+        
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j))return false;
+            i++;j--;
+        }
+        
+        return true;
+        
+    }
+    
+}
