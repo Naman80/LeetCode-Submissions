3dc86e76e58f4efd046622dd1404668ad7bde4e5
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-26 00:47:10 +0530

    Time: 86 ms (26.74%), Space: 44.5 MB (43.68%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,27 +1,41 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         
+        // int n = nums.length;
         int n = nums.length;
-        Integer[][] dp = new Integer[n+1][n+1];
-        return solve(0,-1,nums,dp);
-    }
-    
-    
-    int solve(int idx,int prev,int[]nums,Integer[][] dp){
-        
-        if(idx>=nums.length)return 0;
-        if(prev != -1 && dp[idx][prev] != null)return dp[idx][prev];
-        int np = solve(idx+1,prev,nums,dp);
-        
-        int pick = 0;
-        
-        if(prev == -1 || nums[idx] > nums[prev]){
-            pick  = 1 + solve(idx+1,idx,nums,dp); 
+        int[] dp = new int[n];
+        int ans  = 0;
+        for(int i = 0;i<n;i++){
+            int max = 0;
+            for(int j = 0;j<i;j++){
+                if(nums[i] > nums[j]){
+                max = Math.max(dp[j],max);
+                }   
+            }  
+            dp[i] = max+1;
+            ans = Math.max(ans,dp[i]);
         }
-        if(prev==-1) return Math.max(pick,np);
-        return dp[idx][prev] = Math.max(pick,np);
-        
+        return ans;
+        // Integer[][] dp = new Integer[n+1][n+1];
+        // return solve(0,-1,nums,dp);
     }
+    
+    
+//     int solve(int idx,int prev,int[]nums,Integer[][] dp){
+        
+//         if(idx>=nums.length)return 0;
+//         if(prev != -1 && dp[idx][prev] != null)return dp[idx][prev];
+//         int np = solve(idx+1,prev,nums,dp);
+        
+//         int pick = 0;
+        
+//         if(prev == -1 || nums[idx] > nums[prev]){
+//             pick  = 1 + solve(idx+1,idx,nums,dp); 
+//         }
+//         if(prev==-1) return Math.max(pick,np);
+//         return dp[idx][prev] = Math.max(pick,np);
+        
+//     }
     //     int n = nums.length;
     //     int[] dp = new int[n];
     //     int ans  = 0;
