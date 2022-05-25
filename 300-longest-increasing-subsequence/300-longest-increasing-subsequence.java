4846f03946f7377ed8077866e795fa57cdcc9918
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-26 00:17:22 +0530

    Time: 1112 ms (5.00%), Space: 303.1 MB (5.04%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,5 +1,27 @@
 class Solution {
-    // public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
+        
+        int n = nums.length;
+        Integer[][] dp = new Integer[n+1][n+1];
+        return solve(0,-1,nums,dp);
+    }
+    
+    
+    int solve(int idx,int prev,int[]nums,Integer[][] dp){
+        
+        if(idx>=nums.length)return 0;
+        if(prev != -1 && dp[idx][prev] != null)return dp[idx][prev];
+        int np = solve(idx+1,prev,nums,dp);
+        
+        int pick = 0;
+        
+        if(prev == -1 || nums[idx] > nums[prev]){
+            pick  = 1 + solve(idx+1,idx,nums,dp); 
+        }
+        if(prev==-1) return Math.max(pick,np);
+        return dp[idx][prev] = Math.max(pick,np);
+        
+    }
     //     int n = nums.length;
     //     int[] dp = new int[n];
     //     int ans  = 0;
@@ -18,23 +40,23 @@ class Solution {
     
     
 
-     public int lengthOfLIS(int[] nums) {
-         int n = nums.length;
-         ArrayList<Integer> al = new ArrayList<>();
-         // al.add(nums[0]);
-        int ans= 0;
-         for(int i = 0;i<n;i++){
-             if( al.isEmpty() || nums[i] > al.get(al.size()-1) ){
-                 al.add(nums[i]);
-                 ans++;
-             }else{
-                 int pos =  Collections.binarySearch(al,nums[i]);
-                 if(pos<0){
-                     pos = Math.abs(pos+1);
-                        al.set(pos,nums[i]);
-                 }
-             }  
-         }
-         return ans;
-     }
+     // public int lengthOfLIS(int[] nums) {
+     //     int n = nums.length;
+     //     ArrayList<Integer> al = new ArrayList<>();
+     //     // al.add(nums[0]);
+     //    int ans= 0;
+     //     for(int i = 0;i<n;i++){
+     //         if( al.isEmpty() || nums[i] > al.get(al.size()-1) ){
+     //             al.add(nums[i]);
+     //             ans++;
+     //         }else{
+     //             int pos =  Collections.binarySearch(al,nums[i]);
+     //             if(pos<0){
+     //                 pos = Math.abs(pos+1);
+     //                    al.set(pos,nums[i]);
+     //             }
+     //         }  
+     //     }
+     //     return ans;
+     // }
 }
