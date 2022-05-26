861cd0177669c8b5166312955637f2d90b929e68
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-26 09:04:55 +0530

    Time: 1 ms (75.75%), Space: 41.4 MB (39.75%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -0,0 +1,14 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans=0;
+        
+        for(int i = 0 ;i<=31;i++){
+            
+            if(((n>>i)&1) == 1)ans++;
+            
+        }
+        
+        return ans;
+    }
+}
