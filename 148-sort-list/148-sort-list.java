74bce2551068be7ad67df64c676eb66a0c980fdc
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-28 13:44:54 +0530

    Time: 16 ms (35.39%), Space: 79.2 MB (7.81%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -28,6 +28,7 @@ class Solution {
         
         return merge(part1 , part2);
     }
+    // findind middle of linked list
     
     ListNode findMid(ListNode temp){
         
@@ -44,16 +45,14 @@ class Solution {
         
     }
     
+    // merging two sorted linked list
+    
     ListNode merge(ListNode p1 , ListNode p2){
-        
         if(p1==null)return p2;
         if(p2==null) return p1;
-        
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
-        
         while(p1!=null && p2!=null){
-            
             if(p1.val>p2.val){
                 temp.next = p2;
                 temp = p2;
@@ -63,18 +62,11 @@ class Solution {
                 temp = p1;
                 p1= p1.next;
             }
-            
         }
-        
-        while(p2!=null){
+        if(p1==null){
             temp.next = p2;
-                temp = p2;
-                p2 = p2.next;
-        }
-        while(p1 != null){
-              temp.next = p1;
-                temp = p1;
-                p1= p1.next;
+        }else if(p2 == null){
+            temp.next = p1;
         }
         return dummy.next;
     }
