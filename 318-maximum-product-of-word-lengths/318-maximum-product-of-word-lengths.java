1d5ee453d026e4ea295f1db1b6930ca271a8b94c
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-30 00:26:38 +0530

    Time: 6 ms (99.26%), Space: 42.4 MB (88.77%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,47 +1,27 @@
 class Solution {
-    public int maxProduct(String[] words) {
-//         HashSet<Character> hs  = new HashSet<>();
-//         int n = words.length;
-//         int max = 0;
-//         for(int i= 0;i<n-1;i++){
-//             hs.clear();
-//             String out = words[i];
-//             for(int x =0;x<out.length();x++){
-//                 hs.add(out.charAt(x));
-//             }
-//             for(int j = i+1;j<n;j++){
-//                 boolean check =  true;
-//                 String out2 = words[j];
-//                 for(int x =0;x<out2.length();x++){
-//                 if(hs.contains(out2.charAt(x))){
-//                     check = false;
-//                     break;
-//                 }
-//             }
-//                 if(check){
-//                     max = Math.max(max,out.length() * out2.length());
-//                 }
-//             }
-//         }
+    public int maxProduct(String[] w) {
         
-//         return max;
-        if (words == null || words.length == 0)
-		return 0;
-	int len = words.length;
-	int[] value = new int[len];
-	for (int i = 0; i < len; i++) {
-		String tmp = words[i];
-		value[i] = 0;
-		for (int j = 0; j < tmp.length(); j++) {
-			value[i] |= 1 << (tmp.charAt(j) - 'a');
-		}
-	}
-	int maxProduct = 0;
-	for (int i = 0; i < len; i++)
-		for (int j = i + 1; j < len; j++) {
-			if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
-				maxProduct = words[i].length() * words[j].length();
-		}
-	return maxProduct;
+        int n = w.length;
+        int[] bits = new int[n];
+        for(int i = 0;i<n;i++){
+            String o = w[i];
+            int ol = o.length();
+            
+            for(int j = 0;j<ol;j++){
+                  bits[i] |= (1 << (o.charAt(j) - 'a'));
+            }
+         
+        }
+        
+         int max =0;
+            for(int i = 0;i<n-1;i++){
+                for(int j = i+1;j<n;j++){
+                    if((bits[i] & bits[j]) == 0){
+                        max = Math.max(max , w[i].length() * w[j].length());
+                    }
+                }
+            }
+            
+            return max;
     }
 }
