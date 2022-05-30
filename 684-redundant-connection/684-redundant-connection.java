4359c488c9427a2d67463ffb609ffe7aa0109ec3
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-30 15:48:28 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (91.69%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.java
@@ -0,0 +1,40 @@
+class Solution {
+    int[] parent;
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        parent = new int[n+1];
+        
+        for(int i = 0 ; i<n+1;i++){
+            parent[i] = i;
+        }
+        int ans = 0;
+        for(int i = 0; i<n;i++){
+           int u = edges[i][0];
+           int v = edges[i][1];
+            
+            if(findPar(u) != findPar(v))union(u,v);
+            else ans = i;
+            
+        }
+        
+        return edges[ans];
+    }
+    
+    
+    int findPar(int x){
+        if(parent[x] == x)return x;
+        
+        return parent[x] = findPar(parent[x]);
+    }
+    
+    void union(int u,int v){
+        
+        u = findPar(u);
+        v = findPar(v);
+        
+        if(v!=u){
+            parent[v] = u;
+        }
+        
+    }
+}
