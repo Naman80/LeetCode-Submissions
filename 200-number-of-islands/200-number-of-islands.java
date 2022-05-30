5b9f910c7dff6b9b25e4d9e09ab4c8c94ec3b79a
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-05-30 16:42:44 +0530

    Time: 4 ms (67.65%), Space: 56.9 MB (56.64%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -5,64 +5,52 @@ class Solution {
         int c = grid[0].length;
         for(int i = 0;i<r;i++){
             for(int j = 0;j<c;j++){
-                
                 if(grid[i][j] == '1'){
                     com++;
+                    // bfs(i,j,grid);
                     dfs(i,j,grid);
-                }
-                
+                }       
             }
         }
-        
         return com;
-        
     }
-    
-    
-    // void bfs(int i,int j , char[][]grid){
-    //     int r = grid.length;
-    //     int c = grid[0].length;
-    //     Queue<int[]> q = new LinkedList<>();
-    //     grid[i][j] = '0';
-    //     q.add(new int[]{i,j});
-    //     while(!q.isEmpty()){
-    //         int[] o = q.poll();
-    //         i = o[0];
-    //         j = o[1];
-    //         if(i+1<r && grid[i+1][j]=='1'){
-    //             grid[i+1][j] = '0';
-    //             q.add(new int[]{i+1,j});
-    //         }
-    //         if(j+1 < c && grid[i][j+1] == '1'){
-    //             grid[i][j+1] = '0';
-    //             q.add(new int[]{i,j+1});
-    //         }
-    //        if(i-1>=0 && grid[i-1][j]=='1'){
-    //             grid[i-1][j] = '0';
-    //             q.add(new int[]{i-1,j});
-    //         }
-    //          if(j-1 >=0 && grid[i][j-1] == '1'){
-    //             grid[i][j-1] = '0';
-    //             q.add(new int[]{i,j-1});
-    //         }
-    //     }
-    // }
+    void bfs(int i,int j , char[][]grid){
+        int r = grid.length;
+        int c = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        grid[i][j] = '0';
+        q.add(new int[]{i,j});
+        while(!q.isEmpty()){
+            int[] o = q.poll();
+            i = o[0];
+            j = o[1];
+            if(i+1<r && grid[i+1][j]=='1'){
+                grid[i+1][j] = '0';
+                q.add(new int[]{i+1,j});
+            }
+            if(j+1 < c && grid[i][j+1] == '1'){
+                grid[i][j+1] = '0';
+                q.add(new int[]{i,j+1});
+            }
+           if(i-1>=0 && grid[i-1][j]=='1'){
+                grid[i-1][j] = '0';
+                q.add(new int[]{i-1,j});
+            }
+             if(j-1 >=0 && grid[i][j-1] == '1'){
+                grid[i][j-1] = '0';
+                q.add(new int[]{i,j-1});
+            }
+        }
+    }
     
     void dfs(int i,int j,char[][] grid){
         int r = grid.length;
         int c = grid[0].length;
+        if(i>=r || j>=c || i<0 || j<0 || grid[i][j] == '0') return;
         grid[i][j] = '0';
-         if(i+1<r && grid[i+1][j]=='1'){
-             dfs(i+1,j,grid);
-         }
-         if(j+1 < c && grid[i][j+1] == '1'){
-             dfs(i,j+1,grid);
-         }
-        if(i-1>=0 && grid[i-1][j]=='1'){
-            dfs(i-1,j,grid);
-        }
-          if(j-1 >=0 && grid[i][j-1] == '1'){
-              dfs(i,j-1,grid);
-          }
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
     }
 }
