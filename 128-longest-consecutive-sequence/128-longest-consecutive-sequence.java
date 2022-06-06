1f65f3982bd3be82f9353b8efbc261bb8a6cc56f
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-06 13:28:17 +0530

    Time: 12 ms (98.09%), Space: 51.4 MB (95.66%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -9,14 +9,12 @@ class Solution {
         
         for(int i = 1 ; i < nums.length;i++){
             if(nums[i] == nums[i-1])continue;
-            System.out.print(nums[i] + " ");
             if(nums[i-1] +1 == nums[i]){
                 temp++;
             }else{
                 ans = Math.max(ans,temp);
                 temp = 1;
             }
-            
         }
         ans = Math.max(ans,temp);
         return ans;
