da0dfb9449fd543e97eeb8cce54872cb7820a580
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-06 17:59:11 +0530

    Time: 36 ms (8.02%), Space: 74.8 MB (10.73%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,30 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+    
         HashSet<Integer> hs = new HashSet<>();
-        
-        
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
-        for(int i = 0;i<=n;i++){
-            adj.add(new ArrayList<Integer>());
-        }
+        HashMap<Integer,Integer> hm = new HashMap<>();
         int l = trust.length;
+        if(l==0 && n==1)return 1;
         for(int i = 0 ; i <l;i++){
-            adj.get(trust[i][0]).add(trust[i][1]);
             hs.add(trust[i][0]);
+            hm.putIfAbsent(trust[i][1],0);
+            hm.put(trust[i][1] , hm.get(trust[i][1]) + 1);
         }
-        
-        // System.out.print(adj);
-        int[] indeg = new int[n+1];
-        
-        for(int i = 1 ;i <=n;i++){
-            for(int x : adj.get(i)){
-                indeg[x]++;
-            }
-        }
-        
         for(int i = 1;i<=n;i++){
-            if(indeg[i]==n-1 && !hs.contains(i))return i;
+            if(hm.containsKey(i) && hm.get(i) == n-1 && !hs.contains(i))return i;
         }
         return -1;
     }
