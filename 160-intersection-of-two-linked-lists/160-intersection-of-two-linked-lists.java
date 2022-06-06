fafc06b79e14a6b973da00477269a110ab33324c
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-06 16:09:30 +0530

    Time: 2 ms (47.19%), Space: 55.3 MB (43.97%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,40 +11,46 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-       ListNode temp = headA;
-        int l1 = 0;
-        while(temp!=null){
-            l1++;
-            temp = temp.next;
-        }
-        int l2 = 0 ;
-        temp = headB;
-        while(temp!=null){
-            l2++;
-            temp = temp.next;
-        }
-        int d;
-        ListNode temp2 ;
-        if(l2>l1){
-            d = l2 -l1;
-            temp = headB;
-            temp2 = headA;
-        }else{
-            d = l1 - l2;
-            temp = headA;
-            temp2 = headB;
-        }
+    //    ListNode temp = headA;
+    //     int l1 = 0; //length of first LL
+    //     while(temp!=null){
+    //         l1++;
+    //         temp = temp.next;
+    //     }
+    //     int l2 = 0 ; // length of second LL
+    //     temp = headB;
+    //     while(temp!=null){
+    //         l2++;
+    //         temp = temp.next;
+    //     }
+    //     int d;
+    //     ListNode temp2 ;
+    //     if(l2>l1){ //finding difference between them
+    //         d = l2 -l1;
+    //         temp = headB;
+    //         temp2 = headA;
+    //     }else{
+    //         d = l1 - l2;
+    //         temp = headA;
+    //         temp2 = headB;
+    //     }
+    //     while(d-- > 0){ // covering the extra difference
+    //         temp = temp.next;
+    //     }
+    //     while(temp != null){ 
+    //         if(temp == temp2) return temp;
+    //         temp = temp.next;
+    //         temp2 = temp2.next;
+    //     }
+    //     return null;
+    // }
         
-        while(d-- > 0){
-            temp = temp.next;
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a!=b){
+            if(a==null) a = headB;else a=a.next;
+            if(b==null) b = headA;else b=b.next;   
         }
-        
-        while(temp != null){
-            if(temp == temp2) return temp;
-            temp = temp.next;
-            temp2 = temp2.next;
-        }
-        
-        return null;
+        return a;
     }
 }
