491f5fd7ee8b7df570201ac806ca20db8fdf32e2
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-09 13:13:00 +0530

    Time: 7 ms (11.27%), Space: 50.2 MB (30.42%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -4,11 +4,7 @@ class Solution {
         int n = nums.length;
         for(int i = 0 ;i<n-1;i++){
             int subtar = tar - nums[i];
-            // System.out.println(subtar);
-            // nums[i] = Integer.MAX_VALUE;
             int in = binarySearch(i+1,n-1,nums,subtar);
-            
-            // System.out.println(in);
             if(in>=0){
                 ans[0] = i+1;
                 ans[1] = in+1;
@@ -19,9 +15,6 @@ class Solution {
     }
     
     int binarySearch(int s ,int e , int[] nums,int find){
-        
-       
-        
         while(s<=e){
              int mid = s + (e-s)/2;
             if(nums[mid] == find)return mid;
@@ -29,8 +22,6 @@ class Solution {
             else s = mid+1;
             
         }
-        
         return -1;
-        
     }
 }
