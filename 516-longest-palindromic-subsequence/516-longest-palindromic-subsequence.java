881559123140e58485bfa900173144701f04ed33
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-14 17:32:28 +0530

    Time: 140 ms (15.40%), Space: 71.7 MB (25.13%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,36 +1,54 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        String s2 = reverse(s);
-        int[][] dp = new int[n+1][n+1];
+//     public int longestPalindromeSubseq(String s) {
+//         int n = s.length();
+//         String s2 = reverse(s);
+//         int[][] dp = new int[n+1][n+1];
         
-        for(int i = 0 ; i < n+1;i++){
-            for(int j = 0 ;j< n+1 ;j++){
-                if(i==0 || j ==0)continue;
-                if(s.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+//         for(int i = 0 ; i < n+1;i++){
+//             for(int j = 0 ;j< n+1 ;j++){
+//                 if(i==0 || j ==0)continue;
+//                 if(s.charAt(i-1) == s2.charAt(j-1)){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 }else{
+//                     dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+//                 }
+//             }
+//         }
+        
+//         return dp[n][n];
+        
+//     }
+    
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) return 0;
+        if(s.length() == 1) return 1;
+        int n = s.length();
+        int[][] dp = new int[n + 2][n + 2];
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > 0; j--) {
+                if (s.charAt(i - 1) == s.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j + 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j + 1]);
                 }
             }
         }
-        
-        return dp[n][n];
-        
+        return dp[s.length()][1];
     }
     
-    String reverse(String s){
+    
+//     String reverse(String s){
         
-        String ans = "";
+//         String ans = "";
         
-        int i = s.length() -1;
+//         int i = s.length() -1;
         
-        while(i>=0){
-            ans+= s.charAt(i);
-            i--;
-        }
+//         while(i>=0){
+//             ans+= s.charAt(i);
+//             i--;
+//         }
         
-        return ans;
+//         return ans;
         
-    }
+//     }
 }
