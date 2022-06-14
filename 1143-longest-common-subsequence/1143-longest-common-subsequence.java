3a67b11dd1304a26951c3e7d2d910c2735a122bc
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-14 09:08:01 +0530

    Time: 19 ms (52.90%), Space: 45.5 MB (92.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,21 +4,23 @@ class Solution {
         int n = t1.length();
         int m = t2.length();
         
-        int[][] dp = new int[n+1][m+1];
+        int[] dp = new int[m+1];
         
         for(int i = 1;i<n+1;i++){
+            int[] temp = new int[m+1];
             for(int j = 1;j<m+1;j++){
                 char ch = t1.charAt(i-1);
                 char ch1 = t2.charAt(j-1);
                 if(ch == ch1){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    temp[j] = 1 + dp[j-1];
                 }else{
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                    temp[j] = Math.max(temp[j-1],dp[j]);
                 }
             }
+            
+            dp = temp;
         }
-        
-        return dp[n][m];
-        
+        return dp[m];
+
     }
 }
