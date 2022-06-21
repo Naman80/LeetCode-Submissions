24492987bc4b3803a0ffbefc50a64328bee05685
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-21 15:14:35 +0530

    Time: 255 ms (56.29%), Space: 41.3 MB (54.17%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -0,0 +1,34 @@
+class Solution {
+    int ans = Integer.MAX_VALUE;
+    public int distributeCookies(int[] cook, int k) {
+        
+    int[] arr = new int[k];    
+        solve(0,cook,arr,k);
+    return ans;
+    }
+    
+    
+    public void solve(int idx , int[] cook , int[] arr , int k){
+        
+        if(idx>=cook.length){
+            int max = Integer.MIN_VALUE;
+            for(int i = 0 ; i<k;i++){
+                if(arr[i]==0)return;
+                max = Math.max(max,arr[i]);
+            }
+            ans= Math.min(ans,max);
+            return;
+        }
+        
+        
+        for(int i =0;i<k;i++){
+            arr[i]+=cook[idx];
+            solve(idx+1,cook,arr,k);
+            arr[i]-=cook[idx];
+        }
+        
+    }
+    
+    
+    
+}
