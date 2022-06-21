5a86fd6bc95ed2feecdbfe7544095542baea486a
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-22 00:41:31 +0530

    Time: 7 ms (35.45%), Space: 54.7 MB (70.38%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -5,46 +5,29 @@ class Solution {
         for(int i = 0 ; i < n ;i++){
             adj.add(new ArrayList<>());
         }
-        
         for(int i = 0 ; i < n ; i++){
             for(int  j = 0; j < graph[i].length;j++){
                 adj.get(i).add(graph[i][j]);
             }
         }
-        
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] vis = new boolean[n];
-        
-        dfs(0,adj,vis,ans,new ArrayList<>());
-        
-        return ans;
-        
-        
+        dfs(0,adj,ans,new ArrayList<>());
+        return ans;   
     }
     
     
-    void dfs(int v ,List<List<Integer>> adj, boolean[] vis,List<List<Integer>> ans,List<Integer> al){
-        
-        vis[v] = true;
-        
+    void dfs(int v ,List<List<Integer>> adj,List<List<Integer>> ans,List<Integer> al){
+     
         al.add(v);
-        // System.out.println(al);
         if(v == adj.size()-1){
             ans.add(new ArrayList<>(al));
-            // al.remove(al.size()-1);
-            // vis[v] = false;
-            // return;
-        }
-        
+        }       
         for(int x : adj.get(v)){
-            if(!vis[x]){
-                dfs(x,adj,vis,ans,al);
-            }
+            
+                dfs(x,adj,ans,al);
+            
         }
-        
         al.remove(al.size()-1);
-        
-        vis[v] = false;
-        
+       
     }
 }
