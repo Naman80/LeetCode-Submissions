0a007e38c8c9221dae798972156f6cb8abf9ac4c
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-24 10:40:51 +0530

    Time: 1649 ms (5.10%), Space: 41.6 MB (66.27%) - LeetHub

---
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+    public int minimumTimeRequired(int[] jobs, int k) {
+        
+    int[] persons = new int[k];    
+        
+        return solve(0,k,jobs,persons);
+        // return ans;
+        
+    }
+    
+    
+    
+    
+    
+   int solve(int start,int k,int[] jobs,int[] persons){
+        if(start>=jobs.length){
+            int max = Integer.MIN_VALUE;
+            for(int i = 0 ; i < k;i++){
+                max = Math.max(max,persons[i]);
+            }
+            
+            return max;
+        }
+       int ans = Integer.MAX_VALUE;
+        for(int i = 0 ; i < k ; i++){
+            if(i>0 && persons[i-1]==0)break;
+            persons[i]+= jobs[start];
+            int temp = solve(start+1,k,jobs,persons);
+            persons[i]-= jobs[start];
+            ans = Math.min(temp,ans);
+        }
+       return ans;
+    }
+  
+    
+    
+    
+}
