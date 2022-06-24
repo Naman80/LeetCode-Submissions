e62b76be560a2c403de3a288b9a695438a7ddd49
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-24 15:45:16 +0530

    Time: 1004 ms (10.04%), Space: 40 MB (90.29%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -1,29 +1,47 @@
 class Solution {
-    int min = Integer.MAX_VALUE;
+    int ans = Integer.MAX_VALUE;
     public int minimumTimeRequired(int[] jobs, int k) {
         int[] persons = new int[k];    
-        solve(jobs.length-1,k,jobs,persons);  
-        return min;
+        solve(0,jobs,persons,k);  
+        return ans;
     }
-   void solve(int start,int k,int[] jobs,int[] persons){
-        int max = getMax(persons);
-        // if(max>=min)return;
-        if(start<0){
-            min = Math.min(min,max);
+   // void solve(int start,int k,int[] jobs,int[] persons){
+   //      int max = getMax(persons);
+   //      if(max>=min)return;
+   //      if(start<0){
+   //          min = Math.min(min,max);
+   //          return;
+   //      }
+   //      for(int i = 0 ; i < k ; i++){
+   //          if(i>0 && persons[i-1]==0)break;
+   //          persons[i]+= jobs[start];
+   //          solve(start-1,k,jobs,persons);
+   //          persons[i]-= jobs[start];   
+   //      }
+   //  }
+   //  int getMax(int[] persons){   
+   //      int max = Integer.MIN_VALUE;
+   //      for(int x : persons){
+   //          max = Math.max(max,x);
+   //      }
+   //      return max;
+   //  }
+    
+    public void solve(int idx , int[] cook , int[] arr , int k){
+        if(idx>=cook.length){
+            int max = Integer.MIN_VALUE;
+            for(int i = 0 ; i<k;i++){
+                if(arr[i]==0)return;
+                max = Math.max(max,arr[i]);
+            }
+            ans= Math.min(ans,max);
             return;
+        }   
+        for(int i =0;i<k;i++){
+            if(i>0 && arr[i-1]==0)break;
+            arr[i]+=cook[idx];
+            solve(idx+1,cook,arr,k);
+            arr[i]-=cook[idx];
         }
-        for(int i = 0 ; i < k ; i++){
-            if(i>0 && persons[i-1]==0)break;
-            persons[i]+= jobs[start];
-            solve(start-1,k,jobs,persons);
-            persons[i]-= jobs[start];   
-        }
-    }
-    int getMax(int[] persons){   
-        int max = Integer.MIN_VALUE;
-        for(int x : persons){
-            max = Math.max(max,x);
-        }
-        return max;
     }
 }
