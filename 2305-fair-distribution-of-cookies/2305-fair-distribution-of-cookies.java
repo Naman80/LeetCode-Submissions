df16e76c5a58cd4758384eb578bf12ed67dff1cf
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-24 15:48:38 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (33.66%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,29 +1,38 @@
 class Solution {
     int ans = Integer.MAX_VALUE;
     public int distributeCookies(int[] cook, int k) {
-        
-    int[] arr = new int[k];    
-        solve(0,cook,arr,k);
+    int[] arr = new int[k];
+    solve(0,cook,arr,k);
     return ans;
     }
-    
-    
     public void solve(int idx , int[] cook , int[] arr , int k){
-        
+        // if(idx>=cook.length){
+        //     int max = Integer.MIN_VALUE;
+        //     for(int i = 0 ; i<k;i++){
+        //         if(arr[i]==0)return;
+        //         max = Math.max(max,arr[i]);
+        //     }
+        //     ans= Math.min(ans,max);
+        //     return;
+        // }   
+        int max = getMax(arr);
+        if(max>=ans)return;
         if(idx>=cook.length){
-            int max = Integer.MIN_VALUE;
-            for(int i = 0 ; i<k;i++){
-                if(arr[i]==0)return;
-                max = Math.max(max,arr[i]);
-            }
-            ans= Math.min(ans,max);
+            ans = Math.min(ans,max);
             return;
-        }   
+        }
         for(int i =0;i<k;i++){
             if(i>0 && arr[i-1]==0)break;
             arr[i]+=cook[idx];
             solve(idx+1,cook,arr,k);
             arr[i]-=cook[idx];
         }
+    }
+     int getMax(int[] persons){   
+        int max = Integer.MIN_VALUE;
+        for(int x : persons){
+            max = Math.max(max,x);
+        }
+        return max;
     }
 }
