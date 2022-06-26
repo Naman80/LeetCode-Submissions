933b049812692d0beb88d909ffd2742e4081defc
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-26 13:30:14 +0530

    Time: 10 ms (34.91%), Space: 46.1 MB (33.10%) - LeetHub

--- 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.java
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.java
@@ -15,30 +15,25 @@
  */
 class Solution {
     public int[] findFrequentTreeSum(TreeNode root) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        HashMap<Integer,Integer> hm = new LinkedHashMap<>();
         int totSum = solve(root, hm);
         int max  = Integer.MIN_VALUE;
-        for(Integer x : hm.keySet()){
-            max = Math.max(hm.get(x),max);
-        }
         int count = 0;
-        
         for(Integer x : hm.keySet()){
-            if(hm.get(x)==max)count++;
+            if(hm.get(x) > max){
+                max = hm.get(x);
+                count=1;
+            }else if(hm.get(x)==max)count++;
         }
         int[] ans = new int[count];
         int i = 0;
         for(Integer x : hm.keySet()){
             if(hm.get(x)==max){
-                ans[i] = x;i++;
+                ans[i++] = x;
             }
-            
         }
-        return ans;
-        
+        return ans;   
     }
-    
-    
     int solve(TreeNode root , HashMap<Integer,Integer> hm){
         if(root==null)return 0 ;
         int left = solve(root.left,hm);
