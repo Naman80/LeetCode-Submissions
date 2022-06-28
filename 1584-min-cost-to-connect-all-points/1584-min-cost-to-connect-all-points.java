a26d95b081f1542d7ee7b209007f8f9c965b8bed
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-06-28 14:00:14 +0530

    Time: 1646 ms (5.45%), Space: 224.9 MB (5.02%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -9,48 +9,32 @@ class Solution {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0;i<n;i++){
             for(int j = i+1;j<n;j++){
-                
                 ArrayList<Integer> al = new ArrayList<>();
-                
                 al.add(i);al.add(j);
                 int dis = Math.abs(p[i][0] - p[j][0]) + Math.abs(p[i][1] - p[j][1]);
                 al.add(dis);
-                adj.add(new ArrayList<>(al));
+                adj.add(al);
             }
         }
         
         Collections.sort(adj,Comparator.comparingInt((a)->a.get(2)));
-        
         int sum = 0;
-        
         for(ArrayList<Integer> x : adj){
-            
             if(find(x.get(0)) != find(x.get(1))){
                 sum+=x.get(2);
                 union(x.get(0),x.get(1));
             }
-            
         }
-        
         return sum;
-        
-        
-    
     }
-    
     int find(int x){
-        if(parent[x] == x)return x;
-        
+        if(parent[x] == x)return x;   
         return parent[x] = find(parent[x]);
     }
-    
-    void union(int u , int v){
-        
+    void union(int u , int v){   
         u = find(u);
         v = find(v);
-        
         if(u!=v){
-            
             if(rank[u] < rank[v]){
                 parent[u] = v;
             }else if(rank[v] < rank[u]){
@@ -58,13 +42,7 @@ class Solution {
             }else{
                 parent[v] = u;
                 rank[u]++;
-            }
-            
-        }
-        
+            }   
+        }   
     }
-    
-    
-    
-    
 }
