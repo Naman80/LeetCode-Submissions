6419c5ba2affda2e28723b0317a3ded0500b5d26
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-02 11:00:30 +0530

    Time: 3 ms (75.52%), Space: 43.7 MB (15.29%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -2,31 +2,27 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] time = new int[n][m];
-        bfs(n,m,grid,time);
+        bfs(n,m,grid);
         int ans = 0;
         for(int i = 0 ;i<n;i++){
             for(int j = 0 ; j < m;j++){
                 if(grid[i][j]==1)return -1;
-                ans = Math.max(ans,time[i][j]);
+                ans = Math.max(ans,grid[i][j]);
             }
-        }
-        return ans; 
+        }if(ans==0)return ans;
+        return ans-3; 
     }
     
-    void bfs( int n, int m , int[][] grid,int[][] time){
-        
+    void bfs( int n, int m , int[][] grid){
         Queue<Pair> q = new LinkedList<>();
-        
         for(int r = 0 ; r < n ; r++){
             for(int c = 0;c<m;c++){
                 if(grid[r][c]==2){
                     q.add(new Pair(r,c));
-                    grid[r][c] = 0;
+                    grid[r][c] = 3;
                 }
             }
         }
-        
         int[] dirx = {1,-1,0,0};
         int[] diry = {0,0,-1,1};
         while(q.size()!=0){
@@ -36,10 +32,10 @@ class Solution {
             for(int  i = 0 ;i<4;i++){
                 int newx = x + dirx[i];
                 int newy = y + diry[i];
-            if( newx<0 || newx >=n || newy<0 || newy >=m) continue;
+                if( newx<0 || newx >=n || newy<0 || newy >=m) continue;
                 if( grid[newx][newy] == 1 ){
-                    time[newx][newy] = time[x][y]+1;
-                    grid[newx][newy] = 0;
+                    grid[newx][newy] = grid[x][y]+1;
+                    // grid[newx][newy] = 0;
                     q.add(new Pair(newx,newy));
                 }
             }
