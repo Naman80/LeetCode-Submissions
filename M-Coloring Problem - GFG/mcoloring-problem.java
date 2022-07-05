c8e800d336813a7b97a90f8e8819d8da7c1a3788
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-05 16:47:59 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.java
+++ M-Coloring Problem - GFG/mcoloring-problem.java
@@ -33,6 +33,15 @@ class solve {
     // Function to determine if graph can be coloured with at most M colours
     // such
     // that no two adjacent vertices of graph are coloured with same colour.
+    
+    //THIS QUESTION IS MORE OF RECURSION AND BACKTRACKING TYPE QUESTION
+    
+    // IN THIS QUES WE ARE TRYING OUT ALL THE POSSIBLE COLORS TO EVERY NODE 
+    // AND IF AT END WE REACH THE LAST NODE THEN RETURN TRUE
+    
+    
+    
+    
     public boolean graphColoring(boolean graph[][], int m, int n) {
         
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -40,67 +49,39 @@ class solve {
         for(int i = 0 ; i < n ; i++){
             adj.add(new ArrayList<>());
         }
-        
         for(int i = 0;i<graph.length;i++){
             for(int j = 0 ; j < graph[0].length ; j++){
-                
                 if(graph[i][j]){
                     adj.get(i).add(j);
                 }
-                
             }
         }
-        
-        
         int[] color = new int[n];
-        
         return solve(0,n,adj,m,color);
-        
-        // return true;
-        
-        
-        
-        
-    }
-    boolean solve(int v ,int n,  ArrayList<ArrayList<Integer>> adj , int m ,int[] color){
-        
-        
-        if( v == n)return true;
-        
-        for(int i = 1 ; i <= m ;i++){
-            
-            
-            if(isSafe(v,color,i,adj)){
-                
-                color[v] = i;
-                
-                if(solve(v+1,n,adj,m,color))return true;
-                
-                color[v] = 0;
-                
-            }
-            
-            
-        }
-        
-        return false;
-        
-        
-        
     }
     
+    // COLORING NODE WISE LIKE 0 -> 1 -> 2 -> 3 -> 4 ...
+    //WE ARE NOT COLORING CONNECTION WISE
+    
+    boolean solve(int v ,int n,  ArrayList<ArrayList<Integer>> adj , int m ,int[] color){
+        if( v == n)return true;
+        for(int i = 1 ; i <= m ;i++){ // CHECKING ALL THE COLORS 
+            if(isSafe(v,color,i,adj)){ // IF COLOR IS SAFE OR NOT
+                color[v] = i;  
+                if(solve(v+1,n,adj,m,color))return true;
+                color[v] = 0;  // BACKTRACKING STEP IMPORTANT
+            }
+        }
+        return false;
+    }
+    
+    // CHECKING IF ANY ADJACENT NODE IS COLOURED WITH THE SAME COLOR OR NOT
     
     boolean isSafe(int v, int[] color , int col , ArrayList<ArrayList<Integer>> adj){
-        
-        
-        
         for(int x : adj.get(v)){
-            if(color[x] == col)return false;
+            if(color[x] == col)return false; // IF SAME COLOR ALREADY THEN THIS COLOR IS NOT ALLOWED
         }
-        
-        return true;
-        
-        
+        return true; //NON OF THE ADJACENT NODE HAVE THIS COLOR
     }
     
     
