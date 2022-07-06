916dbd8eaf5e46322677626ab7c12ce0d60185dc
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-06 20:12:14 +0530

    Added solution - LeetHub

--- Distinct occurrences - GFG/distinct-occurrences.java
+++ Distinct occurrences - GFG/distinct-occurrences.java
@@ -31,14 +31,35 @@ class Solution
     int  subsequenceCount(String S, String T)
     {
         
-            int n = S.length();
+        int n = S.length();
         int m = T.length();
-        Integer[][] dp = new Integer[n][m];
         
-        return solve(S,T ,dp);
-      
+        int[][] dp = new int[m+1][n+1];
         
-	    
+        for(int i = 0 ; i < n+1;i++){
+            dp[0][i] = 1;
+        }
+        
+        for(int i = 0; i < m+1;i++){
+            for(int j = 0; j < n+1 ;j++){
+                if(i==0 || j==0)continue;
+                
+                if(T.charAt(i-1) == S.charAt(j-1)){
+                    dp[i][j] = (dp[i][j-1]%mod + dp[i-1][j-1]%mod)%mod;
+                }else{
+                    dp[i][j] = dp[i][j-1]%mod;
+                }
+                
+            }
+        }
+        
+        return dp[m][n];
+        
+        
+        
+        
+        // Integer[][] dp = new Integer[n][m];
+        // return solve(S,T ,dp);
 	
     }
     
@@ -48,9 +69,9 @@ class Solution
         int n = S.length()-1;
         int m = T.length()-1;
         
-        if(m<0)return 1;
+        if(m < 0)return 1;
         if(n < 0 )return 0;
-        if(dp[n][m]!=null)return dp[n][m];
+        if(dp[n][m]!=null) return dp[n][m];
         
         if(S.charAt(n) == T.charAt(m)){
             return  dp[n][m] = (solve(S.substring(0,n) , T,dp)%mod + solve(S.substring(0,n) , T.substring(0,m),dp)%mod)%mod;
