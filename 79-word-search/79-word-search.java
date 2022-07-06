71cadf5d1462c466ee03b5ddbc08d6a98ee0cb77
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-07 00:12:42 +0530

    Time: 165 ms (55.63%), Space: 41.5 MB (84.09%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,46 @@
+class Solution {
+    boolean ans = false;
+    public boolean exist(char[][] mat, String target) {
+        char ch = target.charAt(0);
+        int n = mat.length;
+        int m  = mat[0].length;
+        int tlen = target.length();
+        boolean[][] vis = new boolean[n][m];
+        for(int i = 0;i<n;i++){
+            for(int j = 0 ; j < m;j++){
+                if(mat[i][j] == ch){
+                    solve(i,j,n,m,0,tlen,mat,target,vis);
+                          if(ans)return ans; 
+                }
+            }
+        }
+        return ans;
+    }
+    
+    
+     void solve(int r ,int c , int n , int m , int idx , int tlen ,char[][] mat , String tar,boolean[][] vis){
+        
+        
+        if(r <0 || c <0 || r>=n || c>=m || vis[r][c] || mat[r][c] != tar.charAt(idx))return ;
+        if(idx==tlen-1){
+            ans = true;return;
+        }
+        vis[r][c] = true;
+        // up
+        solve(r-1,c,n,m,idx+1,tlen,mat,tar,vis);
+        // right
+        if(ans)return; 
+         
+        solve(r,c+1,n,m,idx+1,tlen,mat,tar,vis);
+               if(ans)return; 
+        // left
+        solve(r , c-1,n,m,idx+1,tlen,mat,tar,vis);
+               if(ans)return; 
+        //down
+        solve(r+1,c,n,m,idx+1,tlen,mat,tar,vis);
+               if(ans)return; 
+        vis[r][c] = false;
+    }
+    
+    
+}
