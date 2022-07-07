5deade9d2a84262445063c55b7a03da6a29093cc
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-08 01:10:46 +0530

    Time: 4 ms (67.72%), Space: 47.3 MB (76.85%) - LeetHub

---
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        l1 = reverseLL(l1);
+        l2 = reverseLL(l2);
+        
+        ListNode node = addTwoNumbers1(l1,l2);
+        
+        return reverseLL(node);
+    }
+    
+    
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        int carry = 0;
+        
+        while(temp1!=null || temp2 != null){
+            int sum = 0;
+            if(temp1 != null)sum+=temp1.val;
+            if(temp2 != null)sum+=temp2.val;
+            sum+=carry;
+            int length = (int) (Math.log10(sum) + 1);
+            if(length == 2){
+                int st = sum%10;
+                carry = sum/10;
+                sum = st;
+            }
+            if(length==1)carry =0;
+            ListNode add = new ListNode(sum);
+            temp.next = add;
+            temp = temp.next;
+            if(temp1 != null)temp1 = temp1.next;
+            if(temp2 != null)temp2 = temp2.next;
+        }
+        if(carry!=0){
+            ListNode add = new ListNode(carry);
+            temp.next = add;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+    
+    ListNode reverseLL(ListNode node){
+        ListNode prev = null;
+        ListNode curr = node;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;   
+    }
+    
+    
+}
