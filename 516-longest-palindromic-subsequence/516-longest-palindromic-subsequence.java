a8b847fe034881fa1cce7fa1c607e968c31d0f6c
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-11 20:03:45 +0530

    Time: 194 ms (6.98%), Space: 74 MB (8.37%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
-//     public int longestPalindromeSubseq(String s) {
-//         int n = s.length();
-//         String s2 = reverse(s);
-//         int[][] dp = new int[n+1][n+1];
-//         for(int i = 0 ; i < n+1;i++){
-//             for(int j = 0 ;j< n+1 ;j++){
-//                 if(i==0 || j ==0)continue;
-//                 if(s.charAt(i-1) == s2.charAt(j-1)){
-//                     dp[i][j] = 1 + dp[i-1][j-1];
-//                 }else{
-//                     dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
-//                 }
-//             }
-//         }
-//         return dp[n][n];
-//     }
-    
     public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        String s2 = reverse(s);
+        int[][] dp = new int[n+1][n+1];
+        for(int i = 0 ; i < n+1;i++){
+            for(int j = 0 ;j< n+1 ;j++){
+                if(i==0 || j ==0)continue;
+                if(s.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+    
+    // public int longestPalindromeSubseq(String s) {
         // if (s == null || s.length() == 0) return 0;
         // if(s.length() == 1) return 1;
         // int n = s.length();
@@ -31,32 +31,32 @@ class Solution {
         //     }
         // }
         // return dp[s.length()][1];
-        int n = s.length();
-        Integer[][] dp = new Integer[n+1][n+1];
-        return recursive(s,0,n-1,dp);
-    }
-    
-    
-    int recursive(String s ,int st , int e,Integer[][] dp){
-        
-        if(st==e)return 1;
-        if(st>e)return 0;
-        
-        if(dp[st][e] != null)return dp[st][e];
-        if(s.charAt(st) == s.charAt(e)){
-            return dp[st][e] = 2 + recursive(s,st+1,e-1,dp);
-        }else{
-            return dp[st][e] = Math.max(recursive(s,st,e-1,dp),recursive(s,st+1,e,dp));
-        }
-    }
-    
-//     String reverse(String s){ 
-//         String ans = "";
-//         int i = s.length() -1;
-//         while(i>=0){
-//             ans+= s.charAt(i);
-//             i--;
-//         }
-//         return ans;
+//         int n = s.length();
+//         Integer[][] dp = new Integer[n+1][n+1];
+//         return recursive(s,0,n-1,dp);
 //     }
+    
+    
+//     int recursive(String s ,int st , int e,Integer[][] dp){
+        
+//         if(st==e)return 1;
+//         if(st>e)return 0;
+        
+//         if(dp[st][e] != null)return dp[st][e];
+//         if(s.charAt(st) == s.charAt(e)){
+//             return dp[st][e] = 2 + recursive(s,st+1,e-1,dp);
+//         }else{
+//             return dp[st][e] = Math.max(recursive(s,st,e-1,dp),recursive(s,st+1,e,dp));
+//         }
+//     }
+    
+    String reverse(String s){ 
+        String ans = "";
+        int i = s.length() -1;
+        while(i>=0){
+            ans+= s.charAt(i);
+            i--;
+        }
+        return ans;
+    }
 }
