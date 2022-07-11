bb473b3f33508c2255d1d260d599a53cb0d15359
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-11 16:52:34 +0530

    Time: 28 ms (23.56%), Space: 44.6 MB (27.98%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,21 +1,37 @@
 class Solution {
     public int countSubstrings(String s) {
        int n = s.length();
-        int sum = 0;
-        for(int i = 0;i<n;i++){
-          for(int j = i;j<n;j++){
-              if(isPal(s.substring(i,j+1)))sum++;
-          }
+        
+        boolean[][] dp =  new boolean[n][n];
+        int count = 0;
+        for(int g = 0 ; g < n ; g++){
+            
+            for(int i = 0 , j = g ; j < n ; i++,j++){
+                
+                
+                if(i==j){
+                    dp[i][j] = true;
+                    count++;
+                }else if(Math.abs(i-j)==1){
+                       if(s.charAt(i) == s.charAt(j)){
+                           dp[i][j] = true;
+                           count++;
+                       }
+                }
+                else{
+                    
+                    if(s.charAt(i) == s.charAt(j)){
+                        if(dp[i+1][j-1]){
+                            dp[i][j] = true;
+                            count++;
+                        }
+                    }   
+                }
+            }
+            
         }
-        return sum;
-    }
-    boolean isPal(String s){
-        int i =0;
-        int j = s.length()-1;
-        while(i<j){
-            if(s.charAt(i) != s.charAt(j))return false;
-            i++;j--;
-        }
-        return true;
+        
+        return count;
+        
     }
 }
