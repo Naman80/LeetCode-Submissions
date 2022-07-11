b023179ae3ef4c79cc72488d1900eb3d6a56dc44
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-11 20:28:04 +0530

    Time: 104 ms (45.38%), Space: 68.2 MB (70.60%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        String s2 = reverse(s);
-        int[][] dp = new int[n+1][n+1];
-        for(int i = 0 ; i < n+1;i++){
-            for(int j = 0 ;j< n+1 ;j++){
-                if(i==0 || j ==0)continue;
-                if(s.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][n];
-    }
+    // public int longestPalindromeSubseq(String s) {
+    //     int n = s.length();
+    //     String s2 = reverse(s);
+    //     int[][] dp = new int[n+1][n+1];
+    //     for(int i = 0 ; i < n+1;i++){
+    //         for(int j = 0 ;j< n+1 ;j++){
+    //             if(i==0 || j ==0)continue;
+    //             if(s.charAt(i-1) == s2.charAt(j-1)){
+    //                 dp[i][j] = 1 + dp[i-1][j-1];
+    //             }else{
+    //                 dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+    //             }
+    //         }
+    //     }
+    //     return dp[n][n];
+    // }
     
     // public int longestPalindromeSubseq(String s) {
         // if (s == null || s.length() == 0) return 0;
@@ -50,13 +50,51 @@ class Solution {
 //         }
 //     }
     
-    String reverse(String s){ 
-        String ans = "";
-        int i = s.length() -1;
-        while(i>=0){
-            ans+= s.charAt(i);
-            i--;
-        }
-        return ans;
-    }
+    // String reverse(String s){ 
+    //     String ans = "";
+    //     int i = s.length() -1;
+    //     while(i>=0){
+    //         ans+= s.charAt(i);
+    //         i--;
+    //     }
+    //     return ans;
+    // }
+    
+    //GAP STRATEGY
+     public int longestPalindromeSubseq(String s) {
+         
+         
+         int n = s.length();
+         
+         int[][] dp = new int[n][n];
+         
+         for(int g = 0 ; g < n ; g++){
+             for(int i = 0 , j = g ; j < n ;j++,i++){
+                 
+                 if(g==0){
+                     dp[i][j] = 1;
+                 }else if(g==1){
+                     
+                     if(s.charAt(i) == s.charAt(j)){
+                         dp[i][j] = 2;
+                     }else dp[i][j] = 1;
+                     
+                 }else{
+                     
+                     if(s.charAt(i) == s.charAt(j)){
+                         dp[i][j] = dp[i+1][j-1] +2;
+                     }else{
+                         dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+                     }
+                     
+                 }
+                 
+             }
+         }
+         
+         
+         return dp[0][n-1];
+         
+         
+     }
 }
