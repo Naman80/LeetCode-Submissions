d575450bd5529b1f9b387b646f5e6e27ccb8e879
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-11 19:33:49 +0530

    Time: 7 ms (61.40%), Space: 42.3 MB (52.42%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,37 +1,51 @@
-class Solution {
+// class Solution {
+//     public int countSubstrings(String s) {
+//        int n = s.length();
+//         boolean[][] dp =  new boolean[n][n];
+//         int count = 0;
+//         for(int g = 0 ; g < n ; g++){
+//             for(int i = 0 , j = g ; j < n ; i++,j++){
+//                 if(i==j){
+//                     dp[i][j] = true;
+//                     count++;
+//                 }else if(Math.abs(i-j)==1){
+//                        if(s.charAt(i) == s.charAt(j)){
+//                            dp[i][j] = true;
+//                            count++;
+//                        }
+//                 }
+//                 else{
+//                     if(s.charAt(i) == s.charAt(j)){
+//                         if(dp[i+1][j-1]){
+//                             dp[i][j] = true;
+//                             count++;
+//                         }
+//                     }   
+//                 }
+//             }   
+//         }
+//         return count;
+//     }
+// }
+
+
+public class Solution {
+    int count = 0;
+    
     public int countSubstrings(String s) {
-       int n = s.length();
+        if (s == null || s.length() == 0) return 0;
         
-        boolean[][] dp =  new boolean[n][n];
-        int count = 0;
-        for(int g = 0 ; g < n ; g++){
-            
-            for(int i = 0 , j = g ; j < n ; i++,j++){
-                
-                
-                if(i==j){
-                    dp[i][j] = true;
-                    count++;
-                }else if(Math.abs(i-j)==1){
-                       if(s.charAt(i) == s.charAt(j)){
-                           dp[i][j] = true;
-                           count++;
-                       }
-                }
-                else{
-                    
-                    if(s.charAt(i) == s.charAt(j)){
-                        if(dp[i+1][j-1]){
-                            dp[i][j] = true;
-                            count++;
-                        }
-                    }   
-                }
-            }
-            
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
         }
         
         return count;
-        
+    }
+    
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++; left--; right++;
+        }
     }
 }
