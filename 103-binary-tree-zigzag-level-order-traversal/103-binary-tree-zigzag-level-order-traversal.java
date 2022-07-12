af6d97508f09a88ddd0692c1802334996544339b
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-12 13:34:34 +0530

    Time: 1 ms (96.21%), Space: 43.2 MB (11.99%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -16,31 +16,74 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
+        
         List<List<Integer>> ans = new ArrayList<>();
-        if(root==null) return ans;
-        Queue<TreeNode> q  = new LinkedList<>();
+        
+        if(root==null)return ans;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        int count = 0;
         
         q.add(root);
-        boolean flag = true;
+        
+        
         while(q.size()!=0){
-            List<Integer> temp = new ArrayList<>();
-            int size = q.size();
-          
-            while(size-- > 0){
-                
-                  TreeNode out = q.poll();
-                if(out.left!=null) q.add(out.left);
-                if(out.right!=null) q.add(out.right);
-                
-                if(flag) temp.add(out.val);
-                else temp.add(0,out.val);
-                
-            }
             
-            ans.add(temp);
-            flag = !flag;
+            int size = q.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0 ;i < size ; i++){
+                TreeNode out = q.poll();
+                if(count%2==0){
+                    temp.add(out.val);
+                }else{
+                    temp.add(0,out.val);
+                }
+                
+            if(out.left!=null)q.add(out.left);
+            if(out.right!=null)q.add(out.right);
+            }
+            count++;
+            ans.add(new ArrayList<>(temp));
             
         }
+        
         return ans;
+        
+        
+        
+        
+        
+//         List<List<Integer>> ans = new ArrayList<>();
+//         if(root==null) return ans;
+//         Queue<TreeNode> q  = new LinkedList<>();
+        
+//         q.add(root);
+//         boolean flag = true;
+//         while(q.size()!=0){
+//             List<Integer> temp = new ArrayList<>();
+//             int size = q.size();
+          
+//             while(size-- > 0){
+                
+//                   TreeNode out = q.poll();
+//                 if(out.left!=null) q.add(out.left);
+//                 if(out.right!=null) q.add(out.right);
+                
+//                 if(flag) temp.add(out.val);
+//                 else temp.add(0,out.val);
+                
+//             }
+            
+//             ans.add(temp);
+//             flag = !flag;
+            
+//         }
+//         return ans;
+        
+        
+        
+        
+        
     }
 }
