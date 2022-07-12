c5e83a482fc04a9971cfcfaddaf048d526e992ad
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-12 19:07:38 +0530

    Time: 20 ms (82.25%), Space: 42.6 MB (21.11%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean makesquare(int[] nums) {
+        int k = 4;
+         int sum = 0;
+        for(int x:nums){
+            sum+=x;
+        }
+        if(sum%k!=0)return false;
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0 ; i <= n/2;i++){
+            int temp = nums[i];
+            nums[i] = nums[n-1-i];
+            nums[n-1-i] = temp;
+        }
+        int[] arr = new int[k];
+        return solve(0,nums,arr,k,sum/k);
+    }
+    boolean solve(int start,int[] nums,int[] arr,int k,int sum){
+        if(start>=nums.length){
+            for(int i = 1 ;i<k;i++){
+                if(arr[i-1] != arr[i])return false;
+            }
+            return true;   
+        }
+        if(start>= nums.length)return false;
+        for(int i = 0 ;i<k;i++){
+            if(i>0 && arr[i-1]==0)break;
+            arr[i]+=nums[start];
+            if(arr[i]<= sum){
+                 if(solve(start+1, nums,arr,k,sum))return true;
+            }
+            arr[i]-=nums[start];
+            // if(arr[i]==0)break;
+        }
+        
+        return false;
+    }
+    
+}
