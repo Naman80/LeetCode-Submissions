7d5ca06be4958cac50ca030728d0bbab56a821eb
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-21 13:36:50 +0530

    Time: 2 ms (46.82%), Space: 42.4 MB (25.49%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -2,22 +2,35 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int slen = s.length();
         int tlen = t.length();
-        int[][] dp = new int[slen+1][tlen+1];
+        // int[][] dp = new int[slen+1][tlen+1];
+        // for(int i = 1 ; i< slen+1 ; i++){
+        //     for(int j = 1 ; j< tlen + 1 ;j++){
+        //         if(s.charAt(i-1) == t.charAt(j-1)){
+        //             dp[i][j] = 1 + dp[i-1][j-1] ;
+        //         }else{
+        //             dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); 
+        //         }
+        //     }
+        // }
+        // if(dp[slen][tlen] == slen)return true;
+        // return false;
         
-        for(int i = 1 ; i< slen+1 ; i++){
-            for(int j = 1 ; j< tlen + 1 ;j++){
-                
-                if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1] ;
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); 
-                }
+        
+        int count = 0;
+        
+        int i = 0 ; int j = 0;
+        
+        while(i<slen && j<tlen){
+            
+            if(s.charAt(i) == t.charAt(j)){
+                count++;
+                i++;
             }
+            j++;
+            
         }
         
-        if(dp[slen][tlen] == slen)return true;
-        return false;
-        
+        return count==slen;
     }
     
     
