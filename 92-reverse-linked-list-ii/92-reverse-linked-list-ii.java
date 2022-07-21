f1e3c4614188f76c96dbce5d8901c9638fb6574f
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-22 00:59:55 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (13.41%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -1,97 +1,52 @@
-// /**
-//  * Definition for singly-linked list.
-//  * public class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode() {}
-//  *     ListNode(int val) { this.val = val; }
-//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//  * }
-//  */
-// class Solution {
-//     public ListNode reverseBetween(ListNode head, int left, int right) {
-        
-//         if(left == right || head == null || head.next == null)return head;
-        
-        
-//         ListNode dummy  = new ListNode(-1);
-//         dummy.next= head;
-//         ListNode prev1 = dummy;
-// //         left--;
-// //         right--;
-        
-//         while(--left > 0){
-//             prev1 = prev1.next;
-//         }
-        
-//         ListNode first = prev1.next;
-        
-//         ListNode prev2 = dummy;
-        
-//         while(--right > 0){
-//             prev2 = prev2.next;
-//         }
-        
-//         ListNode second = prev2.next;
-        
-        
-//         prev1.next = first.next; // deleting both nodes
-//         prev2.next = second.next;
-        
-        
-//         second.next = prev1.next; //adding nodes again
-//         prev1.next = second;
-        
-//         first.next = prev2.next;
-//         prev2.next = first;
-        
-        
-//         return dummy.next;
-        
-        
-//     }
-// }
-
-
 class Solution {
+
+    // Object level variables since we need the changes
+    // to persist across recursive calls and Java is pass by value.
+    private boolean stop;
+    private ListNode left;
+
+    public void recurseAndReverse(ListNode right, int m, int n) {
+
+        // base case. Don't proceed any further
+        if (n == 1) {
+            return;
+        }
+
+        // Keep moving the right pointer one step forward until (n == 1)
+        right = right.next;
+
+        // Keep moving left pointer to the right until we reach the proper node
+        // from where the reversal is to start.
+        if (m > 1) {
+            this.left = this.left.next;
+        }
+
+        // Recurse with m and n reduced.
+        this.recurseAndReverse(right, m - 1, n - 1);
+
+        // In case both the pointers cross each other or become equal, we
+        // stop i.e. don't swap data any further. We are done reversing at this
+        // point.
+        if (this.left == right || right.next == this.left) {
+            this.stop = true;            
+        }
+
+        // Until the boolean stop is false, swap data between the two pointers
+        if (!this.stop) {
+            int t = this.left.val;
+            this.left.val = right.val;
+            right.val = t;
+
+            // Move left one step to the right.
+            // The right pointer moves one step back via backtracking.
+            this.left = this.left.next;
+        }
+    }
+
     public ListNode reverseBetween(ListNode head, int m, int n) {
-
-        // Empty list
-        if (head == null) {
-            return null;
-        }
-
-        // Move the two pointers until they reach the proper starting point
-        // in the list.
-        ListNode cur = head, prev = null;
-        while (m > 1) {
-            prev = cur;
-            cur = cur.next;
-            m--;
-            n--;
-        }
-
-        // The two pointers that will fix the final connections.
-        ListNode con = prev, tail = cur;
-
-        // Iteratively reverse the nodes until n becomes 0.
-        ListNode third = null;
-        while (n > 0) {
-            third = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = third;
-            n--;
-        }
-
-        // Adjust the final connections as explained in the algorithm
-        if (con != null) {
-            con.next = prev;
-        } else {
-            head = prev;
-        }
-
-        tail.next = cur;
+        this.left = head;
+        this.stop = false;
+        this.recurseAndReverse(head, m, n);
         return head;
     }
 }
