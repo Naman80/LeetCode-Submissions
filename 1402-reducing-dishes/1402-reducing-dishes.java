8dc05b58c3cf8250d34c64344128d7ae72adf152
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-22 12:29:58 +0530

    Time: 9 ms (32.61%), Space: 42.5 MB (41.04%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -2,16 +2,27 @@ class Solution {
     public int maxSatisfaction(int[] sat) {
         Arrays.sort(sat);
         int n = sat.length;
-        Integer[][] dp = new Integer[n][n+1];
-        return solve(0,1,sat,dp);   
-    }
-    int solve(int idx , int time , int[] sat,Integer[][] dp){
-        if(idx >= sat.length){
-            return 0;
+        int[][] dp = new int[n+1][n+2];
+        // return solve(0,1,sat,dp);   
+        
+        
+        for(int idx = n-1 ; idx >=0 ; idx--){
+            for(int time = n ; time >=1 ; time--){
+                int pick = (sat[idx] * time) + dp[idx+1][time+1];
+                int np = dp[idx+1][time];
+                dp[idx][time] = Math.max(np,pick);
+            }
         }
-        if(dp[idx][time]!=null)return dp[idx][time];
-        int pick = (sat[idx] * time) + solve(idx+1 , time+1 , sat,dp);
-        int np = solve(idx+1,time,sat,dp);
-        return dp[idx][time] = Math.max(np,pick);
+        
+        return dp[0][1];
     }
+    // int solve(int idx , int time , int[] sat,Integer[][] dp){
+    //     if(idx >= sat.length){
+    //         return 0;
+    //     }
+    //     if(dp[idx][time]!=null)return dp[idx][time];
+    //     int pick = (sat[idx] * time) + solve(idx+1 , time+1 , sat,dp);
+    //     int np = solve(idx+1,time,sat,dp);
+    //     return dp[idx][time] = Math.max(np,pick);
+    // }
 }
