441e0429b20e471c60941f37fbf3c714bf072731
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-22 12:22:09 +0530

    Time: 49 ms (6.91%), Space: 57.7 MB (5.40%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,27 +1,17 @@
 class Solution {
     public int maxSatisfaction(int[] sat) {
-        
         Arrays.sort(sat);
         int n = sat.length;
-        Integer[][] dp = new Integer[n+1][n+2];
-        return solve(0,1,sat,dp);
-        
+        Integer[][] dp = new Integer[n+1][n+1];
+        return solve(0,1,sat,dp);   
     }
-    
-    
-    
     int solve(int idx , int time , int[] sat,Integer[][] dp){
-        
         if(idx >= sat.length){
             return 0;
         }
         if(dp[idx][time]!=null)return dp[idx][time];
         int pick = (sat[idx] * time) + solve(idx+1 , time+1 , sat,dp);
-        
         int np = solve(idx+1,time,sat,dp);
-        
-        
         return dp[idx][time] = Math.max(np,pick);
-        
     }
 }
