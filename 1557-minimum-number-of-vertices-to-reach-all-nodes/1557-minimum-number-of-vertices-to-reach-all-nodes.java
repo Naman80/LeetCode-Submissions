24850af0101d7feda55122ac6e15aae6f872bc13
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-22 19:39:46 +0530

    Time: 40 ms (25.19%), Space: 118 MB (83.39%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -5,41 +5,27 @@ class Solution {
         for(int i = 0 ;i<n;i++){
             parent[i] = i;
         }
-        
         List<Integer> al = new ArrayList<>();
-        
         for(List<Integer> x : edges){
             int u = x.get(0);
             int v = x.get(1);
             union(u,v);
         }
-        
-        
         for(int i = 0 ;i<n;i++){
             if(parent[i] == i){
                 al.add(i);
             }
         }
-        
-        return al;
-        
-        
-        
+        return al;   
     }
-    
-    
     int find(int x){
         if(parent[x] == x)return x;
-        
         return parent[x] = find(parent[x]);
     }
     void union(int u ,int v){
-        
         int up = find(u);
         int vp = find(v);
-        // System.out.println(u + " " + v);
         if(vp==v)
             parent[vp] = up;
-        
     }
 }
