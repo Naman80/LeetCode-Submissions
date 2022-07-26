e9810a23fd847960865ea1c8f71c9e8f0869f901
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-26 09:54:05 +0530

    Time: 21 ms (35.30%), Space: 49.2 MB (31.67%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -1,16 +1,44 @@
+// LETS CREATE SEGMENT TREE OF THIS ARRAY
+// SPLIT APPRY AT ALL INDEXES
+// CREATE NEW SEGMENT TREE ARRAY OF SIZE (4N)  <-- MAX SIZE
+// LOW HIGH POS <-- PARAMETERS WHILE CREATE ST
+
+
+
+
+// 107549
+
+
 class NumArray {
-    int[] nums1;
-    public NumArray(int[] nums) {
-        nums1 = nums;
-    }
-    
-    public int sumRange(int left, int right) {
-        int sum  = 0;
-        for(int i = left ; i <=right ;i++){
-            sum+=nums1[i];
+    int[] segtree;
+    int n;
+    public int createSegTree(int low , int high , int pos , int[] nums){
+        if(low == high){
+            return segtree[pos] = nums[low];
         }
-        
-        return sum;
+        int mid = low + (high - low)/2;
+        int left = createSegTree(low , mid , 2*pos+1 ,nums);
+        int right = createSegTree(mid+1 , high , 2*pos+2,nums);
+        return segtree[pos] = left + right;   
+    }
+    public int querySum(int ql , int qr , int low , int high , int pos){
+        if(ql <= low && high <= qr)return segtree[pos];
+        if(high < ql ||  qr < low)return 0;
+        int mid = low + (high - low)/2;
+        int left = querySum( ql , qr , low , mid , 2*pos+1);
+        int right = querySum( ql , qr , mid+1 , high , 2*pos+2);
+        return  left + right;   
+    } 
+    public NumArray(int[] nums) {
+        n = nums.length;
+        segtree = new int[4*n];
+        createSegTree(0,n-1,0,nums);
+        // for(int i= 0 ; i < segtree.length ; i++){
+            // System.out.print(segtree[i] + " ");
+        // }
+    }
+    public int sumRange(int left, int right) {
+        return querySum(left , right , 0 , n-1 , 0);
     }
 }
 
