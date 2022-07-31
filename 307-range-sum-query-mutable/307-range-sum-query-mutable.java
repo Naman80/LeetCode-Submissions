1219a4459735458b2e89bf873a3deca8216785e1
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-07-31 07:05:53 +0530

    Time: 148 ms (52.19%), Space: 128.9 MB (51.09%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,56 +1,72 @@
 class NumArray {
-    int[]segTree;
+    
+    int[] segtree;
     int n;
-    
-    public void buildST(int[] nums, int low , int high , int pos){
-        if(low == high){
-            segTree[pos] = nums[low];
-            return ;
-        }
-        int mid  = low + (high - low)/2;
-        buildST(nums , low , mid , 2*pos +1);
-        buildST(nums , mid+1, high , 2*pos +2);
-        segTree[pos] = segTree[2 * pos + 1]  + segTree[2*pos+2];
-    }
-    
-    
-    public int query( int ql , int qh , int low ,int high , int pos){
-        if(ql <= low && high <= qh)return segTree[pos];
-        if(high < ql ||  low  > qh)return 0;
-        int mid = low + (high - low)/2;
-        int left = query( ql , qh , low , mid,2*pos+1);
-        int right = query( ql,qh , mid+1,high , 2*pos+2);
-        return left  + right;
-    }
-    
-    public void change(int low  , int high , int pos , int idx, int val ){
+    void build(int low, int high , int pos , int[] nums){
         
-        if(low==high){
-            segTree[pos] = val;
+        if(low == high){
+            segtree[pos] = nums[low];
             return ;
         }
+        int left = 2 *pos+1;
+        int right = 2*pos+2;
         int mid = low + (high - low)/2;
-        if(idx <= mid)change(low, mid, 2*pos +1,idx,val);
-        else change(mid+1, high , 2*pos+2,idx,val);
-        segTree[pos] = segTree[2 * pos + 1]  + segTree[2*pos+2];
+        build(low , mid , left ,nums);
+        build(mid+1,high , right , nums);
+        segtree[pos] = segtree[left] + segtree[right];  
     }
     
+    int query(int low , int high , int pos , int  l ,int r){
+        if(high < l || low > r)return 0;
+        if(l <= low && high <= r)return segtree[pos];
+        int left = 2 *pos+1;
+        int right = 2*pos+2;
+        int mid = low + (high - low)/2;
+        int ln = query(low, mid , left , l ,r);
+        int rn = query(mid+1,high,right , l,r);
+        return ln+rn;
+    }
+    
+    
+    void change(int low, int high , int pos , int idx , int val){
+    
+        if(low == high){
+            segtree[pos] = val;
+            return;
+        }
+        
+        int left = 2 *pos+1;
+        int right = 2*pos+2;
+        int mid = low + (high - low)/2;
+        
+        if(mid>=idx){
+            change(low , mid , left , idx,val);
+        }else change(mid+1 , high , right, idx ,val);
+        
+        segtree[pos] = segtree[left] + segtree[right];  
+
+        
+        
+        
+    }
+    
+
     public NumArray(int[] nums) {
-        n = nums.length;
-        segTree = new int[4*n];
-        buildST(nums, 0 , n - 1, 0 );
+        int N = nums.length;
+        n = N;
+        segtree = new int[4*N];
+        build(0,n-1,0,nums);
+        
     }
     
     public void update(int index, int val) {
-        
-        change(0,n-1,0,index,val);
-        
-        
+        change(0 , n-1, 0 , index,val);
     }
     
     public int sumRange(int left, int right) {
         
-        return query( left , right , 0  , n -1 ,0);        
+        
+        return query(0,n-1,0,left,right);
     }
 }
 
