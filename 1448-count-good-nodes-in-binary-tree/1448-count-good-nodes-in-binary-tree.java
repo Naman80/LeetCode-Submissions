4a038c3699f9b7ad596607e3fcdf5cda4e83dd4f
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-09-01 21:31:44 +0530

    Time: 3 ms (76.91%), Space: 59.6 MB (64.37%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -16,27 +16,18 @@
 class Solution {
     public int goodNodes(TreeNode root) {
         int[] s = new int[1];
-         solve(root,Integer.MIN_VALUE,s);
+        solve(root,Integer.MIN_VALUE,s);
         return s[0];
     }
     
     void solve(TreeNode root,int prev,int[] store){
-        
         if(root==null)return ;
-        
         if(root.val>=prev)store[0]++;
         if(root.left!=null){
-            
-        solve(root.left , Math.max(root.val,prev),store);
-          
+        solve(root.left , Math.max(root.val,prev),store); 
         }
-        
          if(root.right!=null){
-            
            solve(root.right , Math.max(root.val,prev),store);
-            
         }
-        
-        
     }
 }
