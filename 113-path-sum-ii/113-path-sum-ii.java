9f3f0ed0c319bc5c4693f4c7a541264a73040315
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-09-25 01:24:45 +0530

    Time: 2 ms (85.41%), Space: 44.6 MB (52.76%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -15,39 +15,25 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int tar) {
-        
         List<List<Integer>> ans = new ArrayList<>();
         if(root==null) return ans;
         solve(root,tar,0,ans,new ArrayList<Integer>());
-        
         return ans;
     }
-    
-    public void solve(TreeNode temp , int tar , int sum , List<List<Integer>> ans , List<Integer> al){
-        
+    public void solve(TreeNode temp , int tar , int sum , List<List<Integer>> ans , List<Integer> al){   
         if(sum+temp.val == tar && temp.left == null && temp.right == null){
-            
             al.add(temp.val);
             ans.add(new ArrayList<>(al));
             al.remove(al.size()-1);
             return;
-            
         }
-        
+        al.add(temp.val);
         if(temp.left != null){
-            al.add(temp.val);
             solve(temp.left ,tar,sum+temp.val,ans,al);
-             al.remove(al.size()-1);
-            
         }
-        
         if(temp.right != null){
-            al.add(temp.val);
             solve(temp.right , tar , sum+temp.val , ans , al);
-             al.remove(al.size()-1);
         }
-        
+        al.remove(al.size()-1);
     }
-    
-    
 }
