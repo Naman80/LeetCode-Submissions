9a3d55abc90dbab5ddab5c9a543d34969316a1ac
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-09-29 16:04:48 +0530

    Time: 4 ms (94.54%), Space: 43.7 MB (99.71%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,53 +1,19 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> ans = new ArrayList<>();
-        
-       int idx = 0;
-        int diff = Integer.MAX_VALUE;
-        int n = arr.length;
-        for(int i =0  ; i < n ; i ++){
-            int temp = Math.abs( x - arr[i]);
-            if(temp < diff){
-                diff = temp;
-                idx = i;
-            }
-        }
-        int i = idx ;
-        int j = idx+1;
-        // System.out.println(idx);
-        // if(arr[idx] == x){
-        //     ans.add(x);
-        //     k--;
-        //     i--;
-        // }
-        while(k >0 ){   
-            
-            if(i>=0 && j <n){
-                int temp = Math.abs(arr[i] - x);
-                int temp1 = Math.abs(arr[j] - x);
-                if(temp <= temp1){
-                    ans.add(arr[i]);
-                    i--;
-                }else{
-                    ans.add(arr[j]);
-                    j++;
-                }
-                k--;
-            }
-            else if(k > 0 && i >=0){
-                ans.add(arr[i]);
-                i--;
-                k--;
-            }
-             else if(k > 0 && j < n){
-                ans.add(arr[j]);
-                j++;
-                k--;
-            }
-            
-        }
-        
-        Collections.sort(ans);
-        return ans;
+          int lo = 0;
+		int hi = arr.length - 1;
+		while (hi - lo >= k) {
+			if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
+				lo++;
+			} else {
+				hi--;
+			}
+		}
+		List<Integer> result = new ArrayList<>(k);
+		for (int i = lo; i <= hi; i++) {
+			result.add(arr[i]);
+		}
+		return result;
+    
     }
 }
