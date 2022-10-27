d4054c64a5707a1a04804958fb0e47542ca18651
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-10-27 15:18:49 +0530

    Time: 247 ms (11.85%), Space: 49.6 MB (31.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,28 +2,22 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
         Integer[] dp = new Integer[n];
-        return solve(0 , nums,dp);
+        int ans=solve(0 , nums, dp);
+        if(ans == Integer.MAX_VALUE)return -1;
+        return ans;
     }
-    
-    
-    int solve(int idx , int[] nums,Integer[] dp){
-        
-        if(idx>=nums.length)return Integer.MAX_VALUE;
-        
+    public int solve(int idx , int[] nums, Integer[] dp){
+        if(idx>= nums.length)return Integer.MAX_VALUE;
         if(idx == nums.length - 1)return 0;
         if(dp[idx]!=null)return dp[idx];
-        int ans =  Integer.MAX_VALUE ;
-        
-        for(int i = 1 ; i <= nums[idx] ; i++){ 
-           int temp =  solve(idx+i , nums,dp);
-           ans = Math.min(ans , temp);
-            
+        int ans = Integer.MAX_VALUE;
+        for(int i = 1; i <= nums[idx] ; i++){
+            ans = Math.min(ans , solve(idx+i , nums,dp));
         }
-        
         if(ans == Integer.MAX_VALUE)return dp[idx] = ans;
-        
         return dp[idx] = ans+1;
-        
-        
     }
+    
+    
+    
 }
