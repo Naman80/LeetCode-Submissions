2c986d85cf2a4bca6dc6ba167ed667f835974e29
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-04 20:35:32 +0530

    Time: 298 ms (29.86%), Space: 128.8 MB (5.05%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,33 +1,29 @@
 class Solution {
     public int longestPalindrome(String[] words) {
         HashMap<String , Integer> same = new HashMap<>();
-        
         HashMap<String , Integer> diff = new HashMap<>();
         int n = words.length;
         int ans = 0;
         for(int i = 0 ; i < n ; i ++){
-            
             String temp = words[i];
-            
             if(temp.charAt(0) == temp.charAt(1)){
                 same.putIfAbsent(temp , 0);
                 same.put(temp , same.get(temp)+1);
             }else{
-                
-                StringBuilder sb = new StringBuilder(temp);
-                sb = sb.reverse();
-                String foo = String.valueOf(sb);
-                if(diff.containsKey(foo)){
-                    ans+=4;
-                    diff.put(foo , diff.get(foo)-1);
-                    if(diff.get(foo)==0)diff.remove(foo);
-                    
-                }else{
-                    diff.putIfAbsent(temp,0);
-                    diff.put(temp,diff.get(temp)+1);
-                }
+                diff.putIfAbsent(temp , 0);
+                diff.put(temp , diff.get(temp)+1);
             }
-            
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            StringBuilder sb = new StringBuilder(words[i]);
+            sb = sb.reverse();
+            String temp = String.valueOf(sb);
+               if(diff.containsKey(temp)){
+                   ans+= ((int)Math.min(diff.get(words[i]) , diff.get(temp)) * 2)*2;
+                   diff.remove(words[i]);
+                   diff.remove(temp);
+               }
         }
         boolean odd = false;
         for(String x : same.keySet()){
@@ -35,10 +31,7 @@ class Solution {
             ans+= ((temp/2)*2)*2;
             if(temp%2!=0)odd=true;
         }
-       if(odd)ans+=2; 
-        
+        if(odd)ans+=2; 
         return ans;
-        
-        
     }
 }
