afe5723200b01df97805eb26daebf63aea75e0de
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-05 19:29:57 +0530

    Time: 1061 ms (34.82%), Space: 7.9 MB (79.47%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,3 +1,6 @@
+//**  -> If 1st is true & 2nd is not true, it may be possible that board[i][j] is present somewehere else in the 'board' too
+//For eg, board=[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE" -> board[1][0]=board[1][3]="S"
+
 class Solution 
 {
 public:
@@ -9,26 +12,47 @@ bool helper(int row, int col, vector<vector<char>> &board, int k, string word)//
     if(k==word.size())
        return true;
     // If we are moving outside the board's boundary , we return false
-    if(row>=board.size() || col>=board[0].size() || row < 0 || col<0)
+    if(row>=board.size() || col>=board[0].size() )
        return false;
-
-    bool down,left,right,up = false;
     
-    if(board[row][col] == word[k]){
-        char ch = board[row][col];
-        board[row][col] = '#';
-        up = helper(row-1 , col , board , k+1 , word);
-        if(up)return true;
-        left = helper(row , col-1 , board , k+1 , word);
-        if(left)return true;
-        right = helper(row , col+1 , board , k+1 , word);
-        if(right)return true;
-        down = helper(row+1 , col , board , k+1 , word);
-        if(down)return true;
-        board[row][col] = ch;
-        return false;
-            
-    }else return false;
+    //first going 'down'
+    if(row+1<=board.size() && board[row][col] == word[k])
+    {
+        char ch=board[row][col];
+        board[row][col]='#';
+        if( helper(row+1,col,board,k+1,word)) return true;
+        board[row][col]=ch;
+    }
+
+    //second going 'left'
+    if(col-1>=0 && board[row][col] == word[k])
+    {
+        char ch=board[row][col];
+        board[row][col]='#';
+        if(helper(row,col-1,board,k+1,word)) return true;
+        board[row][col]=ch;
+    }
+
+    //third going 'right'
+    if(col+1<=board[0].size() && board[row][col] == word[k])
+    {
+        char ch=board[row][col];
+        board[row][col]='#';
+        if(helper(row,col+1,board,k+1,word)) return true;
+        board[row][col]=ch;
+    }
+
+    //fourth going 'up'
+    if(row-1>=0 && board[row][col] == word[k])
+    {
+        char ch=board[row][col];
+        board[row][col]='#';
+        if(helper(row-1,col,board,k+1,word)) return true;
+        board[row][col]=ch;
+    }
+
+     //in any other case,
+     return false;
 }
 
 
