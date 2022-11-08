3158946c4dbca0cdb9541b843b0a3c4a30536ac3
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-09 01:31:27 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.java
+++ Minimum sum partition - GFG/minimum-sum-partition.java
@@ -37,34 +37,19 @@ class Solution
 	    boolean[][] dp = new boolean[n+1][sum+1];
 	    dp[0][0] = true;
 	    for(int i = 1 ; i < n+1 ; i++){
-	        
 	        for(int j = 1; j < sum+1 ; j++){
-	            
 	            dp[i][j] = dp[i-1][j];
-	            
 	            if(arr[i-1] <= j){
-	                
 	                dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
-	                
 	            }
-	            
-	            
 	        }
-	        
 	    }
-	    
 	    int min = Integer.MAX_VALUE;
-	    
 	    for(int i = 1 ; i <= sum ; i++){
-	        
 	        if(dp[n][i]){
 	            min = Math.min(min , Math.abs( i - (sum - i)));
 	        }
-	        
 	    }
-	    
 	    return min;
-	    
-	    
 	} 
 }
