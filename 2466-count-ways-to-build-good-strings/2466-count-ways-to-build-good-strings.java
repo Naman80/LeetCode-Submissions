f7a072bcd0e8412f7183f28fa475a94575e4abf9
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-13 02:07:04 +0530

    Time: 58 ms (40.00%), Space: 61.4 MB (20.00%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -2,33 +2,25 @@
 class Solution {
     
     int mod = 1000000007;
-
+    Long[]dp;
     public int countGoodStrings(int low, int high, int zero, int one) {
-        
-        long[] dp  = new long[high+1];
-        
-        dp[0] = 1L;
-        
-        for(int i = 1 ; i < high+1; i++){
-            if(zero<=i){
-                dp[i]+= dp[i - zero]%mod;
-            }
-            
-            if(one<=i){
-                dp[i]+=dp[i - one]%mod;
-            }
-        }
-            long ans = 0;
-        for(int i = low ; i <= high ; i++){
-            ans+= dp[i]%mod;
+        long ans=0;
+        dp = new Long[high+1];
+        for(int i = low  ; i <= high ; i++){
+            ans+= calc(i , zero , one)%mod;
             ans%=mod;
         }
-        
         return (int)ans;
-        
     }
     
- 
     
-    
+    public long calc(int range, int zero, int one){
+        if(range<0)return 0L;
+        if(range==0)return 1L;
+        if(dp[range]!=null)return dp[range];
+        long ans = 0;
+        ans+= calc(range - zero , zero ,one)%mod;
+        ans+= calc(range - one , zero , one)%mod;
+        return dp[range] = ans;
+    }
 }
