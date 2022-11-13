e37cc3df020544f88b23ac4db3468d09a474cc11
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-13 18:54:20 +0530

    Time: 19 ms (13.43%), Space: 47.4 MB (84.26%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,49 +1,35 @@
 class Solution {
+    boolean check = false;
     public boolean validPalindrome(String s) {
-        
+        // adbsdvdsebda
         int n = s.length();
-        int i = 0 ;
-        int j = n-1;
-        int count =0;
-        while(i<=j){
-            char f = s.charAt(i);
-            char l = s.charAt(j);
-            if(f!=l){
-                count = 1;
-                break;
-            }
-            i++;
-            j--;
-        }
-        // if(count==0)return true;
-        int iplus = i+1;
-        int jminus = j-1;
         
-        while(iplus <= j){
-            char f = s.charAt(iplus);
-            char l = s.charAt(j);
-            if(f!=l){
-                count++;
-                break;
+        return solve( 0 , n-1 , s);
+    }
+    
+    public boolean solve(int i , int j , String s){
+        
+        if(i>j)return true;
+        char f = s.charAt(i);
+        char e = s.charAt(j);
+        
+        if(f != e){
+            if(check)return false;
+            else{
+                check = true;
+                return solve(i+1 , j , s) || solve(i , j-1 , s);
+                
             }
-            iplus++;
-            j--;
-        }
-        if(count==1)return true;
-        count=1;
-        while(i <= jminus){
-            char f = s.charAt(i);
-            char l = s.charAt(jminus);
-            if(f!=l){
-                count++;
-                break;
-            }
-            i++;
-            jminus--;
-        }
-        return count<=1;
+        }else return solve(i+1 , j -1 , s);
+        
+        
+        
+        
+        
         
     }
+    
+    
 }
 
 
