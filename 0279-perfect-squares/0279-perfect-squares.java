bd3f803f2c1b5d16747d3a19c4b17f925def0abc
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-22 23:23:51 +0530

    Time: 154 ms (26.28%), Space: 45 MB (27.36%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numSquares(int n) {
+        ArrayList<Integer> al = new ArrayList<>();
+    
+        perf(al);
+        
+        int[] dp = new int[n+1];
+        
+        dp[1] = 1;
+        int size = al.size();
+        for(int i = 2 ; i < n+1;i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0;j<size ; j++){
+                int in = al.get(j);
+                if(in <=  i){
+                    min = Math.min(min,dp[i - in]);
+                }   
+            }
+            dp[i] = min+1;
+        }
+        return dp[n];
+    }
+    
+    
+    void perf(ArrayList<Integer> al){
+        for(int i = 1 ;i <= 100 ; i++){
+            int sqr = i*i;
+            if(sqr > 10000){
+                break;
+            }else{
+                al.add(sqr);
+            }
+        }
+        
+    }
+}
