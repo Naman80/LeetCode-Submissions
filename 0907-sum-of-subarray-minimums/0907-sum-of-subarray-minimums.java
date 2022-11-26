01d10134efa102be23080970e4c2252567f66c63
Naman Goel <56990305+Naman80@users.noreply.github.com>
2022-11-26 16:51:27 +0530

    Time: 220 ms (5.11%), Space: 63.8 MB (78.99%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -5,13 +5,6 @@ class Solution {
         long ans = 0;
         int[] back = nseb(arr);
         int[] front = nsef(arr);
-        // for(int i = 0 ; i < n ; i ++){
-        //     System.out.print(front[i] + " ");
-        // }
-        // System.out.println();
-        // for(int i = 0 ; i < n ; i ++){
-        //     System.out.print(back[i] + " ");
-        // }
         for(int i = 0 ; i < n ; i ++){
             long temp = 0;
             temp+= (i - front[i])%mod;
@@ -31,7 +24,7 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         st.add(n-1);
         for(int i = n - 2 ; i >=0 ; i--){
-            while(st.size()!=0 && arr[i] <= arr[st.peek()]){
+            while(st.size()!=0 && arr[i] < arr[st.peek()]){
                 st.pop();
             }
             if(st.size()!=0){
@@ -48,7 +41,7 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         st.add(0);
         for(int i = 1 ; i<n ; i++){
-            while(st.size()!=0 && arr[i] < arr[st.peek()]){
+            while(st.size()!=0 && arr[i] <= arr[st.peek()]){
                 st.pop();
             }
             if(st.size()!=0){
