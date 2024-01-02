236326f784c74874f8c8490949c6f4d9c47fdaee
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-01-02 10:15:40 +0530

    Time: 35 ms (5.35%), Space: 29.8 MB (83.60%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -3,20 +3,14 @@ public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         int n = nums.size();
         vector<int>map(n+1, 0);
-        int mx = -1;
-        int mxe = -1;
-        for(int i = 0 ; i < n ; i++){            
+        vector<vector<int>> ans;           
+        for(int i = 0 ; i < n ; i++){   
             map[nums[i]]++;
-            mx = max(mx , map[nums[i]]);
-            mxe = max(mxe , nums[i]);
-        }
-        vector<vector<int>> ans(mx);
-        for(int i = 1 ; i <= mxe ; i++ ){
-            for(int j = 0 ; j < map[i] ; j++){
-                ans[j].push_back(i);
+            if(ans.size()<map[nums[i]]){
+                ans.push_back({});
             }
+            ans[map[nums[i]]-1].push_back(nums[i]);
         }
-        
         return ans;
         // tc -> O(n) + O(max_ele * most_freq_ele)
     }
