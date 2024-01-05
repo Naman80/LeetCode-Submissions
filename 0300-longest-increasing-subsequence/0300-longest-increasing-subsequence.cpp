eef892c4a1dfdce7b6116d7ab83b596f8691990a
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-01-05 08:54:07 +0530

    Time: 9 ms (83.99%), Space: 10.6 MB (92.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,18 +2,38 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int dp[n];
-        dp[0] = 1;
-    int ans = 1;
-        for(int i = 1 ; i < n ; i++){
-            int temp = 0;
-            for(int j = 0 ; j < i ; j++){
-                if(nums[j] < nums[i])temp=max(temp , dp[j]);
-            }
-            dp[i] = temp+1;
-            ans = max(ans , dp[i]);
-        }
+//         int dp[n];
+//         dp[0] = 1;
+//         int ans = 1;
+//         for(int i = 1 ; i < n ; i++){
+//             int temp = 0;
+//             for(int j = 0 ; j < i ; j++){
+//                 if(nums[j] < nums[i])temp=max(temp , dp[j]);
+//             }
+//             dp[i] = temp+1;
+//             ans = max(ans , dp[i]);
+//         }
         
-        return ans;
+//         return ans;
+        
+        vector<int> list;
+        
+        list.push_back(nums[0]);
+        for(int i = 1 ; i < n ; i++){
+            
+            if(nums[i] > list[list.size()-1]){
+                list.push_back(nums[i]);
+            }else{
+                auto find = lower_bound(list.begin() , list.end() , nums[i]);
+                if(find == list.end()){
+                    list[list.size()-1] = nums[i];
+                }else{
+                    int idx = find - list.begin();
+                    list[idx] = nums[i];
+                }
+            }
+            
+        }
+        return list.size();
     }
 };
