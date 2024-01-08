ce19eb6fdbee73868918970612021680501449a1
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-01-08 13:50:34 +0530

    Time: 1561 ms (14.72%), Space: 259.1 MB (29.44%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        map<long long , int> map[n+1];
+        int ans = 0;
+        for(int i= 1 ; i < n ;  i ++){
+            for(int j = 0 ; j < i; j ++){
+                long long diff = nums[i]* 1LL - nums[j];
+                int count = map[j][diff];
+                ans+=count;
+                map[i][diff] = count+map[i][diff]+1;
+            }
+        }
+        
+        return ans;
+        
+        
+    }
+};
