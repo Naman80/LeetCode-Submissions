e153c685d892248d7a2abddee49fc1a222936915
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-01-17 08:27:32 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (20.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>map;
+        map<int,int>map;
         for(int x:arr)map[x]++;
-        unordered_set<int>set;
-        for(auto [a,b] : map){
-            set.insert(b);
-        }
+        set<int>set;
+        for(auto [a,b] : map)set.insert(b);
         return set.size() == map.size();
     }
 };
