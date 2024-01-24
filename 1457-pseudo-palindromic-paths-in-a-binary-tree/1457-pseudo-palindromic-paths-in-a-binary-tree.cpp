48fc3078d122647ef749468f7e80042550fa8def
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-01-24 09:07:35 +0530

    Time: 248 ms (87.67%), Space: 177.8 MB (98.12%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -14,20 +14,17 @@ public:
     int ans = 0;
     void solve(TreeNode* root , vector<int>&count){
         if(root == NULL)return;
+        count[root->val]++;
         if(root->left ==NULL && root->right ==NULL){
             int temp = 0;
-            count[root->val]++;
             for(int x : count)if(x&1)temp++;
             count[root->val]--;
-            if(temp>1)return;
-            ans++;
+            if(temp<=1)ans++;
             return;
         }
-        count[root->val]++;
         solve(root->left , count);
         solve(root->right , count);
-        count[root->val]--;
-        
+        count[root->val]--;   
     }
     int pseudoPalindromicPaths (TreeNode* root) {
         vector<int>count(10,0);
