a84d87d1b88fe4443d43bf23e49889c8b9d238d3
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-02-02 09:25:35 +0530

    Time: 3 ms (17.15%), Space: 7.7 MB (8.37%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> ans;
-    void solve(int idx , string temp ,int dig , int low, int high){
+    bool solve(int idx , string temp ,int dig , int low, int high){
         if(idx == dig){
             int bar = stoi(temp);
             if(bar >= low  && bar <= high)ans.push_back(bar);
+            return true;
         }
-        if(idx>dig)return;
+        if(idx>dig)return false;
         int b = temp[temp.size()-1]+1;
-        if(b>57)return;
+        if(b>57)return false;
         string foo(1, temp[temp.size()-1]+1);
-        solve(idx+1,temp+foo,dig,low,high);
+        return solve(idx+1,temp+foo,dig,low,high);
     }
     vector<int> sequentialDigits(int low, int high) {
         int mind = to_string(low).size();
@@ -18,7 +19,7 @@ public:
         for(int j = mind ; j <= maxd ; j++){
             for(int i = 1 ; i <= 9 ; i ++){
                 string temp(1, 48+i);
-                solve(1 ,temp , j, low, high );
+                if(solve(1 ,temp , j, low, high )==false)break;
             }
         }
         return ans;
