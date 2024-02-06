3ed554641e8af024787836133b036f896761c430
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-02-06 13:59:05 +0530

    Time: 54 ms (18.08%), Space: 34.3 MB (5.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,32 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<vector<int>,vector<string>> map;
-        
+        vector<vector<string>>ans;
         for(auto x : strs){
             vector<int> temp(26,0);
             for(char ch : x){
                 temp[ch-'a']++;
             }
-            bool mapcheck = true;
-            for(auto &[a,b] : map){
-                bool check =true;
-                for(int i = 0 ; i < 26 ; i++){
-                    if(a[i] != temp[i]){
-                        check = false;
-                        break;
-                    }
-                }
-                if(check){
-                    b.push_back(x);
-                    mapcheck = false;
-                    break;
-                }
-            }
-            if(mapcheck){
-                map[temp] = {x};
-            }    
+            map[temp].push_back(x);
         }
-        vector<vector<string>>ans;
         for(auto [a,b] : map){
            ans.push_back(b);
         }
