bdc24b627f13c6fc41a2e118419ea715da239e23
Naman Goel <56990305+Naman80@users.noreply.github.com>
2024-02-07 22:20:55 +0530

    Time: 16 ms (19.18%), Space: 10.8 MB (28.61%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,12 +1,26 @@
 class Solution {
+    
+    
 public:
+    struct compareFun{
+        bool operator()(pair<int, char> p1, pair<int, char> p2) {
+            if (p1.first == p2.first) return p1.second > p2.second;
+            return p1.first < p2.first;
+        }
+    };
+    
     string frequencySort(string s) {
-        priority_queue<pair<int,char>> pq;
+      priority_queue<pair<int, char>, vector<pair<int, char>>, compareFun> pq;
         map<char,int>map;
         for(char ch :s)map[ch]++;
         for(auto [a,b] : map)pq.push({b,a});
         string ans = "";
-        while(pq.size()){for(int i =0 ; i < pq.top().first;i++)ans+=pq.top().second;pq.pop();}
+        while(pq.size()){
+            for(int i =0 ; i < pq.top().first;i++){
+                ans+=pq.top().second;
+            }
+            pq.pop();
+        }
         return ans;
     }
 };
